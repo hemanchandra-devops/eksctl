c24d87ad511326551da2e0ad9273a822f243ad1c
heman chandra <hemanchandra701@gmail.com>
2026-08-16 19:36:51 +0530

    k8s

--- workstation.sh
+++ workstation.sh
@@ -22,7 +22,3 @@ PLATFORM=$(uname -s)_$ARCH
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 sudo install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
-
-git clone https://github.com/hemanchandra-devops/eksctl.git
-cd eksctl
-eksctl create cluster --config-file=eks.yaml
